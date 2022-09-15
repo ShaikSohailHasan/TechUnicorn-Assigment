acfe0dd2abb6c29c4b5bf57efa955c6bc87c8d06
SohailHasan <68341057+ShaikSohailHasan@users.noreply.github.com>
2022-09-15 12:29:53 +0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [searchTitle, setSearchTitle] = useState("");
+
+  useEffect(() => {
+    const loadPosts = async () => {
+      setLoading(true);
+      const response = await axios.get(
+        "https://fakestoreapi.com/products"
+      );
+      setPosts(response.data);
+      setLoading(false);
+    };
+
+    loadPosts();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Search Filter</h3>
+      <input
+        style={{ width: "30%", height: "25px" }}
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => setSearchTitle(e.target.value)}
+      />
+      <div className = "products-container">{loading ? (
+        <h4>Loading ...</h4>
+      ) : (
+        posts
+          .filter((value) => {
+            if (searchTitle === "") {
+              return value;
+            } else if (
+              value.title.toLowerCase().includes(searchTitle.toLowerCase())
+            ) {
+              return value;
+            }
+          })
+          .map((item) => {
+            return (
+              <div key={item.id} className="product-item">
+                <div className="card"><img src={item.image} alt="#" /></div>
+                <div className="card-description">
+                  <h4 className="card-category">{item.category}</h4>
+                  <h2 className="card-title">{item.title}</h2>
+                  <h3 className="card-price">{`$${item.price}`}</h3>
+                </div>
+                </div>
+            );
+          })
+      )}</div>
     </div>
   );
 }
